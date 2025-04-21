df313a2d97628508c8a84255a8084cb0a6ac44bf
ameenxo <ameenppm007@gmail.com>
2025-04-22 00:55:49 +0530

    serverFix-socketServer-complete connection

--- server/socketServer.js
+++ server/socketServer.js
@@ -6,7 +6,7 @@ module.exports = (httpServer) => {
         cors: {
             origin: "http://localhost:3000",
             methods: ['GET', "POST"],
-
+            credentials: true,
         }
     })
     io.on('connection', (socket) => {
