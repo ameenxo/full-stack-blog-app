d03de11f02d1e1448638b42c32a2e088859d0535
ameenxo <ameenppm007@gmail.com>
2025-04-25 03:33:21 +0530

    server-userRoute-: created a api for get all users .

--- server/routes/UserRoute.js
+++ server/routes/UserRoute.js
@@ -3,6 +3,10 @@ const { getUserProfile, UpdateUserProfile } = require('../middleware/userOperati
 const sendResponse = require('../utility/sendResponse');
 const upload = require('../middleware/uploadImages');
 const deleteImage = require('../utility/deleteImage');
+const User = require('../models /userModel');
+const { getAllBlogs } = require('../middleware/blogOperations');
+const { getAllUsersExceptLoggedIn } = require('../utility/userUtility');
+const CustomError = require('../utility/customError');
 const UserRoute = express.Router();
 
 
@@ -22,6 +26,17 @@ UserRoute.patch('/profile', upload.single('avatar'), UpdateUserProfile, (req, re
         }
         return sendResponse(res, error.statusCode || 500, true, error.message || "internal server error. updating profile process");
     }
+})
+UserRoute.get('/all', async (req, res) => {
+    getAllUsersExceptLoggedIn(req.user._id)
+        .then((users) => {
+            if (users.length === 0) {
+                throw new CustomError("cannot find any user", 404, "request failed")
+            }
+            return sendResponse(res, 200, false, "fetched all users ", users)
+        }).catch((error) => {
+            return sendResponse(res, error.statusCode || 500, true, error.message || "internal server error ")
+        })
 })
 
 module.exports = UserRoute
