896ede86733cbe6e86c99e723d1c3a052f9cc496
ameenxo <ameenppm007@gmail.com>
2025-04-25 00:22:55 +0530

    server-socketServer-: socket connection,disconnection complete with state

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,6 +1,7 @@
-const { isObjectIdOrHexString } = require('mongoose')
 const { Server } = require('socket.io')
 module.exports = (httpServer) => {
+
+    const onlineUsers = new Map();
 
     const io = new Server(httpServer, {
         cors: {
@@ -9,11 +10,20 @@ module.exports = (httpServer) => {
             credentials: true,
         }
     })
-    io.on('connection', (socket) => {
-        console.log('a client connected ')
-
+    io.on('connection', async (socket) => {
+        const userId = socket.handshake.query.userId;
+        if (!userId) {
+            console.log("Connection rejected: No userId provided");
+            socket.disconnect(); // force disconnect
+            return;
+        }
+        socket.
+        onlineUsers.set(userId, socket.id);
+        console.log("users: ", onlineUsers);
         socket.on('disconnect', (socket) => {
-            console.log("client disconnected :", socket.id);
+            onlineUsers.delete(userId);
+            console.log(`User ${userId} disconnected`);
+            console.log(onlineUsers);
 
         });
     })
