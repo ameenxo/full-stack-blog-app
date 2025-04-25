66704ea765a1b2ef27936f5fa911f57faced9b8b
ameenxo <ameenppm007@gmail.com>
2025-04-26 03:10:00 +0530

    server-socketServer-: created send message function and emit to user

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,4 +1,5 @@
 const { Server } = require('socket.io')
+const Message = require('./models /messageModel')
 module.exports = (httpServer) => {
 
     const onlineUsers = new Map();
@@ -18,12 +19,22 @@ module.exports = (httpServer) => {
             return;
         }
         onlineUsers.set(userId, socket.id);
-        console.log("users: ", onlineUsers);
+
+        socket.on('sendMessage', async ({ senderId, receiverId, text }) => {
+            try {
+                const newMessage = (await Message.create({ sender: senderId, receiver: receiverId, text: text, timestamp: Date.now(), isRead: false })).save()
+                if (onlineUsers.has(receiverId)) {
+                    const receiverSocketId = onlineUsers.get(receiverId);
+                    io.to(receiverSocketId).emit("receiveMessage", newMessage);
+                }
+                callback({ success: true, message: "Message sent", data: newMessage });
+            } catch (error) {
+                callback({ success: false, message: "Failed to send message", error: error.message });
+            }
+        });
+
         socket.on('disconnect', (socket) => {
             onlineUsers.delete(userId);
-            console.log(`User ${userId} disconnected`);
-            console.log(onlineUsers);
-
         });
     })
 
