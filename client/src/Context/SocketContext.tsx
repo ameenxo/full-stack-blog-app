5de14021c0a9a3981db7fd90cc2eb55f1ea4ae77
ameenxo <ameenppm007@gmail.com>
2025-04-26 16:56:46 +0530

    client-socketContext-: context created and provided

--- client/src/Context/SocketContext.tsx
+++ client/src/Context/SocketContext.tsx
@@ -1,22 +1,40 @@
 "use client"
 // contexts/SocketContext.js
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import { createContext, ReactNode, useContext, useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { useAuth } from "./AuthContext"; // or wherever your user context is
 
 
+
+interface ReceivedMessageType {
+    senderId: string;
+    username: string;
+    avatar: string;
+    count: number;
+    lastMessage: {
+        text: string;
+        timestamp: string;
+    };
+}
 interface SocketProviderProps {
     children: ReactNode;
 }
+interface SocketContextType {
+    socket: Socket | null;
+    sendMessage: (receiverId: string, text: string, callback: (response: { success: boolean; message?: string }) => void) => void;
+    receivedMessage: ReceivedMessageType | null;
+}
 
-const SocketContext = createContext<Socket | null>(null);
+const SocketContext = createContext<SocketContextType | null>(null);
 
 export const SocketProvider = ({ children }: SocketProviderProps) => {
     const { user } = useAuth()
     const [socket, setSocket] = useState<Socket | null>(null);
+    const [receivedMessage, setReceivedMessage] = useState<ReceivedMessageType | null>(null)
+    const isListenerSet = useRef(false);
 
     useEffect(() => {
-        if (user && !socket) {
+        if (user) {
             const newSocket = io(process.env.NEXT_PUBLIC_SERVER_API_URL, {
                 withCredentials: true,
                 transports: ["websocket"],
@@ -36,9 +54,27 @@ export const SocketProvider = ({ children }: SocketProviderProps) => {
 
         }
     }, [user]);
-
+    useEffect(() => {
+        if (socket && !isListenerSet.current) {
+            socket.on("receiveMessage", (message: ReceivedMessageType) => {
+                console.log("New message received:", message);
+                setReceivedMessage(message);
+            });
+            isListenerSet.current = true;
+        }
+    }, [socket]);
+    const sendMessage = (receiverId: string, text: string, callback: (response: { success: boolean; message?: string }) => void) => {
+        if (socket) {
+            socket.emit("sendMessage", { senderId: user?._id, receiverId, text }, callback);
+        }
+    };
+    const value: SocketContextType = {
+        socket,
+        sendMessage,
+        receivedMessage,
+    };
     return (
-        <SocketContext.Provider value={socket}>
+        <SocketContext.Provider value={value}>
             {children}
         </SocketContext.Provider>
     );
